774400d52ff154bd89001f489a3153995d6f7eb9
Dimas Akmal <dimasawp13@gmail.com>
2025-10-10 19:27:30 +0700

    chore: initial React 18 + Tailwind 3 setup, update structure to monorepo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import "./App.css";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ frontend/src/services/axiosClient.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+// bila import dan pakai variable api nanti dicek ada token tidak kalau tidak berarti request tanpa token jadinya invalid
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
